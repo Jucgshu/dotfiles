521bb892f2d0a530e8c7b1ebed2f922e864aeda5
Jucgshu <balayop@yahoo.fr>
2022-09-05 21:02:59 +0200

    Add some aliases

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -1,8 +1,15 @@
 # Aliases
-alias ls="ls -N --group-directories-first --color=auto"
-alias tmux="tmux new-session -A -s main"
-alias yayu="yay -Syu --devel --noconfirm"
-alias mpv="gnome-session-inhibit mpv"
+alias ls='ls -N --group-directories-first --color=auto'
+alias l='ls -lah'
+alias tmux='tmux new-session -A -s main'
+alias yayu='yay -Syu --devel --noconfirm'
+alias mpv='gnome-session-inhibit mpv'
+alias mkdir='mkdir -p -v'
+
+# Moving
+alias cd ...='cd ../..'
+alias cd..='cd ..'
+alias :q=' exit'
 
 # Coloring applications
 alias diff='diff --color=auto'

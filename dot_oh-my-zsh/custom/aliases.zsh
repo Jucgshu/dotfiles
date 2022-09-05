b02b71474010c6fe070563c3092b6ddf3b0afa45
Jucgshu <balayop@yahoo.fr>
2022-09-05 19:38:34 +0200

    Change custom directory

---
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,10 @@
+# Aliases
+alias ls="ls -N --group-directories-first --color=auto"
+alias tmux="tmux new-session -A -s main"
+alias yayu="yay -Syu --devel --noconfirm"
+alias mpv="gnome-session-inhibit mpv"
+
+# Coloring applications
+alias diff='diff --color=auto'
+alias grep='grep --color=auto'
+alias ip='ip -color=auto'
